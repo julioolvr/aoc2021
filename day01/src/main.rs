caf7e3b0c3d17c5d9aef71b91313620d76ce398b
Julio <julio.olvr@gmail.com>
2021-12-01 13:22:39 +0100

    Reuse increase count

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -15,64 +15,37 @@ fn main() {
     let part_1 = part_1(lines.iter().cloned());
     println!("Part 1: {}", part_1);
 
-    let part_2 = part_2(lines.iter().cloned());
+    let part_2 = part_2(&lines);
     println!("Part 2: {}", part_2);
 }
 
-fn part_1(mut measurements: impl Iterator<Item = u64>) -> u64 {
-    let first_depth = measurements
+fn part_1(measurements: impl Iterator<Item = u64>) -> u64 {
+    count_increases(measurements)
+}
+
+fn part_2(measurements: &[u64]) -> u64 {
+    count_increases(measurements.windows(3).map(|window| window.iter().sum()))
+}
+
+fn count_increases(mut measurements: impl Iterator<Item = u64>) -> u64 {
+    let first_measurement = measurements
         .next()
         .expect("Unexpected empty measurements iterator");
 
-    let increases = measurements.fold(
-        (0, first_depth),
-        |(increases, previous_depth), current_depth| {
-            let next_increases = if current_depth > previous_depth {
-                increases + 1
-            } else {
-                increases
-            };
+    measurements
+        .fold(
+            (0, first_measurement),
+            |(increases, previous_measurement), current_measurement| {
+                let next_increases = if current_measurement > previous_measurement {
+                    increases + 1
+                } else {
+                    increases
+                };
 
-            (next_increases, current_depth)
-        },
-    );
-
-    increases.0
-}
-
-fn part_2(mut measurements: impl Iterator<Item = u64>) -> u64 {
-    let first_window = (
-        measurements
-            .next()
-            .expect("Unexpected empty measurements iterator"),
-        measurements
-            .next()
-            .expect("Unexpected empty measurements iterator"),
-        measurements
-            .next()
-            .expect("Unexpected empty measurements iterator"),
-    );
-
-    let increases = measurements.fold(
-        (0, first_window),
-        |(increases, previous_window), current_depth| {
-            let previous_window_sum = previous_window.0 + previous_window.1 + previous_window.2;
-            let current_window_sum = previous_window.1 + previous_window.2 + current_depth;
-
-            let next_increases = if current_window_sum > previous_window_sum {
-                increases + 1
-            } else {
-                increases
-            };
-
-            (
-                next_increases,
-                (previous_window.1, previous_window.2, current_depth),
-            )
-        },
-    );
-
-    increases.0
+                (next_increases, current_measurement)
+            },
+        )
+        .0
 }
 
 fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
