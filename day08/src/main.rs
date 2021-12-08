2758222d46e1b83df0cb47e440630d617e3fa095
Julio <julio.olvr@gmail.com>
2021-12-08 22:57:35 +0100

    Day 08: Description and small refactoring

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -7,6 +7,68 @@ use std::{
     str::FromStr,
 };
 
+/**
+ * --- Day 8: Seven Segment Search ---
+ *
+ * Today's challenge involves a seven-segment digits display that is incorrectly wired. A
+ * seven-segment display is made out of seven segments named A to G in this way:
+ *
+ *   aaaa
+ *  b    c
+ *  b    c
+ *   dddd
+ *  e    f
+ *  e    f
+ *   gggg
+ *
+ * The input is a list of "signals" where each signal is made of several digits as its input and
+ * several digits as its output. Each digit in a signal is represented as a set of segments (e.g.
+ * `cgeb`) that are enabled for that digit. The challenge is that the segments in the signal don't
+ * correspond to the drawing above - for each signal, each segment has been improperly wired so
+ * that, for example, in a given signal the segment c might turn on the segment a in the reference
+ * drawing above.
+ *
+ * The input is parsed into three main structures - a `Signal` which has an input and an output,
+ * each of which is a vec of `Digit`s. Each `Digit` is made of a set of enabled `Segment`s, and
+ * `Segment` is simply an enum of letters A through G.
+ *
+ * Part 1 asks for what we call here "trivial" digits, more specifically how many are in the
+ * signals' outputs. Trivial digits are those that, given the amount of segments present in a digit,
+ * it's obvious which number they represent. For instance, if there are two segments enabled for a
+ * digit, that digit could only possibly be a one. The other trivial numbers are 4 (4 segments), 7
+ * (3 segments) and 8 (7 segments). Counting the segments for each digit in the output is enough to
+ * arrive to the solution.
+ *
+ * Part 2 asks for the sum of all the fully-decoded outputs. In order to figure out the exact
+ * mapping for each signal, we'll consider that the schema above is the "correct" position for each
+ * segment - it gives each segment a name. Then let's consider a signal that looks like this:
+ *
+ *  dddd
+ * e    a
+ * e    a
+ *  ffff
+ * g    b
+ * g    b
+ *  cccc
+ *
+ * In this schema, the A segment is mapped to the D "input". In this signal, turning on the A and B
+ * input will highlight the C and F "output" segments, forming a 1.
+ *
+ * The `Guesses` struct is used to find this input/output mapping. We start by saying that each
+ * output could be *any* of the inputs - we don't have any information so far. This is represented
+ * by a `HashMap` from a `Segment` to a `HashSet<Segment>`. Then we start iterating over each digit
+ * in the signal. Given the count of segments on each digit it can only be one of a few possible
+ * numbers. Those numbers define the only possible target segment for each of the segments in that
+ * digit. For instance, with the mapping above, a digit `ab` can only be a 1. In the target, a 1 is
+ * formed by C and F. So now we know that C and F must each be mapped to either A or B. This is
+ * narrowed down by the intersection of the current possibilities for each segment (right now C and
+ * F could be anything) and the ones that correspond to the current digit (A and B). With that
+ * narrowed down we continue with the next digit. Once we identified one input/output pair (let's
+ * say that input A corresponds to output C), we can remove A as a possibility from all other
+ * targets. We continue looping narrowing down the possibilities this way until all targets have
+ * only one input, and use that to decode the output of the signal.
+ */
+
 fn main() {
     let file = if env::args()
         .skip(1)
@@ -55,13 +117,18 @@ impl Signal {
         }
     }
 
+    fn digits(&self) -> impl Iterator<Item = &Digit> {
+        self.input.iter().chain(self.output.iter())
+    }
+
     fn decode_output(&mut self) -> usize {
-        let mut input_iteration = self.input.iter().cycle();
+        let digits: Vec<Digit> = self.digits().cloned().collect();
+        let mut digits_iteration = digits.into_iter().cycle();
 
         while !self.guesses.all_guessed() {
             use Segment::{A, B, C, D, E, F, G};
 
-            let next_input = input_iteration.next().unwrap();
+            let next_input = digits_iteration.next().unwrap();
 
             if let Some(value) = next_input.trivial_value() {
                 match value {
@@ -214,7 +281,7 @@ impl Guesses {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Digit {
     enabled_segments: HashSet<Segment>,
 }
