cd5ee2b69fc9d1a9c3a7d87180f32d1001c81e2a
Julio <julio.olvr@gmail.com>
2021-12-13 10:36:19 +0100

    Day 12: Part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, str::FromStr};
+use std::{collections::HashMap, env, str::FromStr};
 
 fn main() {
     let file = if env::args()
@@ -12,8 +12,11 @@ fn main() {
     };
 
     let mut graph = Graph::new(file.lines().map(|line| line.parse().unwrap()).collect());
-    let part_1 = graph.paths_count();
+    let part_1 = graph.paths_count(1);
     println!("Part 1: {}", part_1);
+
+    let part_2 = graph.paths_count(2);
+    println!("Part 2: {}", part_2);
 }
 
 struct Path {
@@ -35,36 +38,48 @@ impl FromStr for Path {
 
 struct Graph {
     paths: Vec<Path>,
-    visited: Vec<String>,
+    visited: HashMap<String, usize>,
 }
 
 impl Graph {
     fn new(paths: Vec<Path>) -> Self {
         Graph {
             paths,
-            visited: vec![],
+            visited: HashMap::new(),
         }
     }
 
-    fn paths_count(&mut self) -> usize {
-        self.visited = vec![];
-        self.paths_count_from("start", 0)
+    fn paths_count(&mut self, max_visits_for_single_small_cave: usize) -> usize {
+        self.visited = HashMap::new();
+        self.paths_count_from("start", max_visits_for_single_small_cave)
     }
 
-    fn paths_count_from(&mut self, from: &str, i: usize) -> usize {
+    fn paths_count_from(&mut self, from: &str, max_visits_for_single_small_cave: usize) -> usize {
         if from == "end" {
             return 1;
         }
 
-        if !self.visited.contains(&from.to_owned()) && from.chars().all(char::is_lowercase) {
-            self.visited.push(from.to_string());
+        if from.chars().all(char::is_lowercase) {
+            *self.visited.entry(from.to_string()).or_insert(0) += 1;
         }
+
+        let max_allowed_visits = if self
+            .visited
+            .values()
+            .any(|value| *value == max_visits_for_single_small_cave)
+        {
+            1
+        } else {
+            max_visits_for_single_small_cave
+        };
 
         let mut targets: Vec<String> = self
             .paths
             .iter()
             .filter(|path| {
-                path.to != "start" && path.from == from && !self.visited.contains(&path.to)
+                path.to != "start"
+                    && path.from == from
+                    && self.visited.get(&path.to).unwrap_or(&0) < &max_allowed_visits
             })
             .map(|path| path.to.clone())
             .collect();
@@ -73,7 +88,9 @@ impl Graph {
             .paths
             .iter()
             .filter(|path| {
-                path.from != "start" && path.to == from && !self.visited.contains(&path.from)
+                path.from != "start"
+                    && path.to == from
+                    && self.visited.get(&path.from).unwrap_or(&0) < &max_allowed_visits
             })
             .map(|path| path.from.clone())
             .collect();
@@ -82,12 +99,12 @@ impl Graph {
 
         let count: usize = targets
             .iter()
-            .map(|target| self.paths_count_from(target, i + 1))
+            .map(|target| self.paths_count_from(target, max_visits_for_single_small_cave))
             .sum();
 
-        if self.visited.last() == Some(&from.to_string()) {
-            self.visited.pop();
-        }
+        self.visited
+            .entry(from.to_string())
+            .and_modify(|count| *count -= 1);
 
         count
     }
